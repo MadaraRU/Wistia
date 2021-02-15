595c9df2ff4e1fa6c8e4285d4d073e03a1adcceb
MadaraRU <78321649+MadaraRU@users.noreply.github.com>
2021-02-15 18:00:14 +0100

    Wistia landing page

---
+++ Wistia/js/script.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+    $("#nav").mouseenter(function() {
+        $("#na1").css("color", "black");
+        $("#na2").css("color", "black");
+        $("#na3").css("color", "black");
+        $("#na4").css("color", "black");
+        $(".jsx-2111810157").css("fill", "black");
+    });
+});
+
+$(document).ready(function() {
+    $("#nav").mouseleave(function() {
+        $("#na4").css("color", "white");
+        $("#na1").css("color", "white");
+        $("#na2").css("color", "white");
+        $("#na3").css("color", "white");
+        $(".jsx-2111810157").css("fill", "white");
+    });
+});
+
+$(document).ready(function() {
+    $("#na4").mouseover(function() {
+
+        $("#na4").css("color", "#E7198A");
+    });
+});
+$(document).ready(function() {
+    $("#na3").mouseover(function() {
+
+        $("#na3").css("color", "#003DCF");
+    });
+});
+$(document).ready(function() {
+    $("#na2").mouseover(function() {
+
+        $("#na2").css("color", "#00c185");
+    });
+});
+$(document).ready(function() {
+    $("#na1").mouseover(function() {
+
+        $("#na1").css("color", "#58b7ff");
+    });
+});
+$(document).ready(function() {
+    $("#na4").mouseleave(function() {
+
+        $("#na4").css("color", "black");
+    });
+});
+$(document).ready(function() {
+    $("#na3").mouseleave(function() {
+
+        $("#na3").css("color", "black");
+    });
+});
+$(document).ready(function() {
+    $("#na2").mouseleave(function() {
+
+        $("#na2").css("color", "black");
+    });
+});
+$(document).ready(function() {
+    $("#na1").mouseleave(function() {
+
+        $("#na1").css("color", "black");
+    });
+});
